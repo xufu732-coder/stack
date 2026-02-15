a7c670346cbe326ef19703bfe6672258b7a07cf8
xufu732-coder <xufu732@gmail.com>
2026-02-16 03:24:31 +0900

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="会計アプリ", page_icon="🧧")
 st.title("🧧 会計入力アプリ")
 
 # --- 設定：スプレッドシートのURLを直接指定 ---
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1-4WPWmJEAI2jIoE7poCSOEKfqSIFMf2mn_U_EkCQ1X4/edit"
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/1-4WPWmJEAI2jIoE7poCSOEKfqSIFMf2mn_U_EkCQ1X4/edit?usp=sharing"
 
 # Google Sheetsへの接続
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -53,4 +53,5 @@ if submit_button:
         st.balloons()
     except Exception as e:
         st.error(f"登録中にエラーが発生しました: {e}")
+
 
